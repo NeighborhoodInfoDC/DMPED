464cdd353cbbd1222d9e0025d1de5a04d9baa7d6
Samlieb <samwatlieber@gmail.com>
2024-07-01 18:03:01 -0400

    Update acs_data_92.R

--- Prog/Neighborhood change/acs_data_92.R
+++ Prog/Neighborhood change/acs_data_92.R
@@ -7,10 +7,33 @@ readRenviron("~/.Renviron")
 
 #ACS median home value 2018-2022
 
-dc_median_home_value <- 
+dc_median_home_value_22 <- 
 get_acs(geography = "tract",
         variable = "B25107_001",
         year = 2022,
         state = "DC",
         geometry = FALSE)
+
+dc_median_home_value_12 <- 
+  get_acs(geography = "tract",
+          variable = "B25107_001",
+          year = 2012,
+          state = "DC",
+          geometry = FALSE)
+dc_median_rent_22 <- 
+  get_acs(geography = "tract",
+          variable = "B25113_001",
+          year = 2022,
+          state = "DC",
+          geometry = FALSE)
+dc_median_rent_22 <- 
+  get_acs(geography = "tract",
+          variable = "B25113_001",
+          year = 2012,
+          state = "DC",
+          geometry = FALSE)
+
+
+B25113_001
+
 
