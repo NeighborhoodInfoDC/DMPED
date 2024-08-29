7427f9c146233f836f47759bdcea2ac54bc3cc30
Samlieb <samwatlieber@gmail.com>
2024-08-29 16:49:43 -0400

    Update NHPD_data.R

--- Prog/Neighborhood change/NHPD_data.R
+++ Prog/Neighborhood change/NHPD_data.R
@@ -3,4 +3,14 @@
 #see if I can get other NHPD data from 2012 and 2000 after finishing the above section 
 
 #nhpd script
-#
+library(tidyverse)
+library(readxl)
+library(lubridate)
+library(tidycensus)
+###IMPORTANT DEDUPLICATE ALL
+NHPD_Subsidies_Only_Export <- read_excel("C:/Users/slieberman/Downloads/NHPD Subsidies Only Export.xlsx")
+NHPD_Properties_Only_Export <- read_excel("C:/Users/slieberman/Downloads/NHPD Properties Only Export.xlsx")
+View(NHPD_Properties_Only_Export)
+#flag for inactive / inconclusive properties
+compiled_NHPD_data <- left_join(NHPD_Properties_Only_Export, NHPD_Subsidies_Only_Export, by = "NHPD Property ID")
+
