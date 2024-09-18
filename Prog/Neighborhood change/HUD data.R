5d2435ffe52c78eab1d43e21089e021ab3540728
Yipeng Su <ysu@urban.org>
2024-09-17 21:30:34 -0400

    Update HUD data.R

--- Prog/Neighborhood change/HUD data.R
+++ Prog/Neighborhood change/HUD data.R
@@ -17,4 +17,9 @@ library(dplyr)
 library(Hmisc)
 census_api_key("05de4dca638d81abd2dc60d0d28e3781183e185e", install = TRUE)
 
-HUDsubsidy <- read_csv("C:/Users/Ysu/Box/Greater DC/Projects/DMPED Housing Assessment 2024/Task 2 - Nbrhd Change and Displacement Risk Assessment/Data collection/Raw/HUD/TRACT_AK_MN_2023_2020census.csv") 
+HUDsubsidy <- read_xlsx("C:/Users/Ysu/Box/Greater DC/Projects/DMPED Housing Assessment 2024/Task 2 - Nbrhd Change and Displacement Risk Assessment/Data collection/Raw/HUD/TRACT_AK_MN_2023_2020census.xlsx") 
+
+subsidy_2023 <- HUDsubsidy %>% 
+  filter(states=="DC District of Columbia") %>% 
+  select(code,program_label,total_units) %>% 
+  spread(program_label, total_units)
